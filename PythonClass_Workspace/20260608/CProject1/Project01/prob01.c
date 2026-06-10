e032b85547c8e4dc18658a4cfef224929095ce00
Lachesis <98927924+llKatzll@users.noreply.github.com>
2026-06-10 10:09:20 +0900

    %x 비사용, 16진수 한 자리 문자를 하나씩 char로 받아서 직접 계산하기
    창작문제

--- PythonClass_Workspace/20260608/CProject1/Project01/prob01.c
+++ PythonClass_Workspace/20260608/CProject1/Project01/prob01.c
@@ -24,7 +24,7 @@ int main() {
 	약수 : 1 2 3 4 6 12
 	소수가 아닙니다*/
 
-	int num;
+	/*int num;
 	int count = 0;
 
 	scanf_s("%d", &num);
@@ -36,9 +36,6 @@ int main() {
 			count++;
 			printf("%d ", i);
 		}
-		else {
-			continue;
-		}
 	}
 
 	if (count == 2) {
@@ -46,6 +43,34 @@ int main() {
 	}
 	else {
 		printf("\n소수가 아닙니다");
+	}*/
+
+
+	//%x로 자동 변환 하지 말고, 16진수 한 자리 문자를 하나씩 %c로 받아서 직접 계산. (자릿수는 3자리로 고정해도 됨)
+	//(고정하지 않을 경우, 제어문이 필요할 것.)
+	
+	//입력: 1 A 3   (한 글자씩)
+	//출력: 419
+
+	char amount;
+
+	int result = 0;
+	
+	while (scanf_s("%c", &amount, 1) == 1) {
+		if (amount == '\n') break;
+
+		if (amount >= '0' && amount <= '9') {
+			amount = amount - '0';
+		}
+		else if (amount >= 'A' && amount <= 'F') {
+			amount = amount - 'A' +10;
+		}
+
+		result = result * 16 + amount;
 	}
+	
+	printf("%d", result);
+
+
 
 }
