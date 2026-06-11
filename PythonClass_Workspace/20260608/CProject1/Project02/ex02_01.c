e9bcee88b54dce8869a426c7c7c3fa2dec0e4d97
Lachesis <98927924+llKatzll@users.noreply.github.com>
2026-06-11 18:01:22 +0900

    공부

--- PythonClass_Workspace/20260608/CProject1/Project02/ex02_01.c
+++ PythonClass_Workspace/20260608/CProject1/Project02/ex02_01.c
@@ -65,31 +65,140 @@ int main() {
 	//	break;
 	//}
 
-	int code;
-	int amount;
+	//int code;
+	//int amount;
 
-	printf("코드 번호를 입력하세요(1 가정용, 2 산업용, 3 교육용):");
-	scanf_s("%d", &code);
-	printf("전기 사용량을 입력하세요(kw):");
-	scanf_s("%d", &amount);
+	//printf("코드 번호를 입력하세요(1 가정용, 2 산업용, 3 교육용):");
+	//scanf_s("%d", &code);
+	//printf("전기 사용량을 입력하세요(kw):");
+	//scanf_s("%d", &amount);
 
-	switch (code) {
-	case 1: //가정용
-		printf("\n전기요금 : %.2lf",
-			(1130 + (amount * 127.8)) * 1.09);  // base * (1 + 세율)
-		break;
-	case 2: //산업용
-		printf("\n전기요금 : %.2lf",
-			(660 + (amount * 88.5)) * 1.08);
-		break;
-	case 3: //교육용
-		printf("\n전기요금 : %.2lf",
-			(370 + (amount * 52)) * 1.05);
-		break;
-	default:
-		printf("\n잘못된 코드입니다\n");
-		break;
+	//switch (code) {
+	//case 1: //가정용
+	//	printf("\n전기요금 : %.2lf",
+	//		(1130 + (amount * 127.8)) * 1.09);  // base * (1 + 세율)
+	//	break;
+	//case 2: //산업용
+	//	printf("\n전기요금 : %.2lf",
+	//		(660 + (amount * 88.5)) * 1.08);
+	//	break;
+	//case 3: //교육용
+	//	printf("\n전기요금 : %.2lf",
+	//		(370 + (amount * 52)) * 1.05);
+	//	break;
+	//default:
+	//	printf("\n잘못된 코드입니다\n");
+	//	break;
+	//}
+
+	/*do {
+		printf("1~3숫자입력");
+		scanf_s("%d", &a);
+	} while (a < 1 || a > 3);*/
+
+	
+
+	//while (num < 5) {
+	//	printf("%d", num);
+	//	num += 3;
+	//} //testing
+
+	/*int num=0;while(num<5)if(num>=2){break;}else{printf("%d",num++);};*/
+
+	/*
+	while문 사용
+
+	2 * 1 = 2
+	2 * 2 = 4
+	2 * 3 = 6
+	2 * 9 = 18
+	*/
+
+
+	/*int a = 1;
+	while (a <= 9) {
+		printf("2 * %d = %d\n", a, 2 * a);
+		a++;
+	}*/
+
+	/*
+	 while 사용
+
+	1부터 100까지의 합
+	1+2+3+4+......+99+100 (누적:sum=sum+값)
+
+	printf("1부터 100까지의 합:%d\n", sum);
+	*/
+
+	/*int sum = 0;
+	int i = 1;
+	while (i <= 100) {
+		sum += i;
+		i++;
 	}
+	printf("%d", sum);*/
 
-	return 0;
+	/*int dan;
+	int i = 1;*/
+
+	/*printf("한 개의 단을 입력 : ");
+	scanf_s("%d", &dan);
+
+	while (i < 10) {
+		printf("%d * %d - %d\n", dan, i, dan * i);
+		i++;
+	}*/
+
+	//for 배웠다고 함
+
+	/*int input;
+
+	scanf_s("%d", &input);
+
+	for (int i = 1; i <= 9; i++) {
+		printf("\n%d x %d = %d", input, i, input * i);
+
+		if ((input * i) % 2 == 0) {
+			printf(" [짝]");
+		}
+		else {
+			printf(" [홀]");
+		}
+	}*/
+
+	int n, total = 0;
+
+	scanf_s("%d", &n);
+
+	for (int dan = 1; dan <= n; dan++) {      // 바깥: 1~N단
+		for (int i = 1; i <= 9; i++) {        // 안쪽: ×1~×9
+			int result = dan * i;
+			total += result;                  // 총합 누적
+
+			printf("%d x %d = %d ", dan, i, result);   // 10진수
+
+			// 16진수 직접 변환
+			printf("(0x");
+			if (result < 16) {
+				if (result < 10) printf("%c", '0' + result);
+				else             printf("%c", 'A' + result - 10);
+			} else {
+				int high = result / 16;
+				int low  = result % 16;
+				if (high < 10) printf("%c", '0' + high);
+				else           printf("%c", 'A' + high - 10);
+				if (low < 10)  printf("%c", '0' + low);
+				else           printf("%c", 'A' + low - 10);
+			}
+			printf(") ");
+
+			// 짝홀 (삼항)
+			printf("%s\n", (result % 2 == 0) ? "[짝]" : "[홀]");
+		}
+	}
+	printf("총합: %d", total);
+
+
+
+	 return 0;
 }
