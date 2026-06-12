6da490b88426c4ac28626dd0dbf01589754ff7ca
Lachesis <98927924+llKatzll@users.noreply.github.com>
2026-06-12 16:54:09 +0900

    주소값 대비공부
    장모님이 주신 미친개딱딱주소값

--- PythonClass_Workspace/20260608/CProject1/Project03/ex03_02.c
+++ PythonClass_Workspace/20260608/CProject1/Project03/ex03_02.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int sumfunc(int);
-//1부터 n까지
+int main()
+{
+    int anData[4] = { 1,2,3,4 };
+    int  i;
 
-int main() {
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d ", anData[i]); // 1  2  3  4
+    }
 
-	int number;
-	int result;
+    // 모든 배열의 이름은 배열의 시작 주소값이다
+    // 첫 번째 요소(인덱스가 0번인 요소)의 주소이다
+    printf("\n%x  %X", anData, anData); //45affc78  45AFFC78
+    printf("\n%x  %X", anData[0], anData);// 1  45AFFC78
+    printf("\n%x  %X", anData[0], anData[0]); // 1  1
+    printf("\n%x  %X", &anData[0], &anData[1]);// 45affc78  45AFFC7C
 
-	printf("숫자를 입력하세요(정수)");
-	scanf_s("%d", &number); //10
 
-	result = sumfunc(number);
+    printf("\n%p   %P", anData, anData);
+    // 0000007FE1B1FCF8   P
 
-	printf("%d", result);
-}
-
-int sumfunc(int num) {
-
-	int res = 0;
-
-	for (int i = 1; i <= num; i++) {
-		res += i;
-	}
-
-	return res;
+    printf("\n%p   %P", &anData[0], &anData[0]);
+    // 0000007FE1B1FCF8   P
+    return 0;
 }
